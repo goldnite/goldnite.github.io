7a8dcdb04f787e9e7468b76bb50c74b4efa1ab61
goldnite <goldendevens@gmail.com>
2023-05-14 16:55:16 -0400

    update

--- src/components/Menu/SocialIcons/SocialIcons.constants.jsx
+++ src/components/Menu/SocialIcons/SocialIcons.constants.jsx
@@ -1,9 +1,8 @@
 // import cv from "./CV.pdf";
 
 export const socialLinks = [
-  { name: "Twitter", link: "https://twitter.com/_Eduard26" },
-  { name: "Dribbble", link: "https://dribbble.com/Eduard26" },
-  { name: "Github", link: "https://github.com/eduardconstantin" },
-  // {name: "Resume", link: cv},
-  { name: "Mail", link: "mailto: ibinceanu.eduard@yahoo.com" },
+  // { name: "Twitter", link: "https://twitter.com/goldnite" },
+  { name: "Github", link: "https://github.com/goldnite" },
+  // {name: "Resume", link: 'cv'},
+  { name: "Mail", link: "mailto:goldendevens@gmail.com" },
 ];
--- src/helpers/useChangeDocumentTitle.js
+++ src/helpers/useChangeDocumentTitle.js
@@ -2,6 +2,6 @@ import { useEffect } from "react";
 
 export const useChangeDocumentTitle = (pageTitle) => {
   useEffect(() => {
-    document.title = `Jiro Matsumoto | Personal Portfolio - ${pageTitle}`;
+    document.title = `goldnite | Personal Portfolio - ${pageTitle}`;
   }, [pageTitle]);
 };
